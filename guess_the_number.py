915492415141bae440fb9c475edc8df108749877
leshiirudo <shiirudo@outlook.fr>
2022-01-27 23:41:12 +0100

    Several code improvements
    - Added comments to organize code file
    - Removed useless booleans
    - Non case-sensitive difficulty selecting actually works
    - Used addition assignment instead of assigning value to itself 
    - Used \n instead of empty print()
    - Made hard difficulty less hard

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,48 +1,45 @@
 import random
 
-print("Welcome to Guess the Number!")
-print()
+# welcome message
+print("Welcome to Guess the Number!" + "\n")
 
-difficulty_selected = False # using boolean to end loop when difficulty was chosen
-
-while difficulty_selected == False:
+# select difficulty
+while True:
     difficulty = input("Select difficulty (Easy, Normal, Hard): ")
-    difficulty.lower()
+    difficulty = difficulty.lower()
     if difficulty == "easy":
         max_guess = 20
-        difficulty_selected = True
+        print()
+        break
     elif difficulty == "normal":
         max_guess = 100
-        difficulty_selected = True
+        print()
+        break
     elif difficulty == "hard":
-        max_guess = 1000
-        difficulty_selected = True
+        max_guess = 500
+        print()
+        break
     else:
         print("Please select an existing difficulty.")
 
-print()
-
+# initiate game
 number_to_guess = random.randint(1, max_guess)
 number_of_guesses = 1
-game_in_progress = True # using boolean to check if game is still in progress
 
-while game_in_progress == True:
+# run game
+while True:
     player_guess = 0
     try:
         player_guess = int(input("Guess a number between 1 and " + str(max_guess) + ": "))
     except ValueError:
-        print("This isn't a number.")
-        game_in_progress = False # trying to figure out how to go back to start of loop to avoid game over
+        print("This isn't a number. Game over.")
+        break
     if player_guess == number_to_guess:
-        print()
-        print("Congrats! You won in " + str(number_of_guesses) + " tries!")
+        print("\n" + "Congrats! You won in " + str(number_of_guesses) + " tries! Game over.")
         break
     elif player_guess < number_to_guess:
         print("This number is too low! Try higher.")
-        number_of_guesses = number_of_guesses + 1
+        number_of_guesses += 1
     elif player_guess > number_to_guess:
         print("This number is too big! Try lower.")
-        number_of_guesses = number_of_guesses + 1
-
-print()
-print("Game over.")
+        number_of_guesses += 1
